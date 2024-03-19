9c981c0d985b8ab9be34c671827851ee39875b7b
david <a17100641@gmail.com>
2024-03-18 20:24:07 -0600

    nav

--- src/App.js
+++ src/App.js
@@ -1,27 +1,34 @@
 import logo from './imagenes/logo.png';
 import './App.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import './estilos/navbar.css'
+import './estilos/navbar.css';
+
 function App() {
   return (
     <div className="App container-fluid">
       <div className='row'>
         <nav className="navbar navbar-expand-lg navbar-dark barra">
           <div className="container">
-            <img src={logo} alt="Imagen de prueba" className="imagen ml-5" />
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-              <span className="navbar-toggler-icon"></span>
-            </button>
+            <div className="row align-items-center">
+              <div className="col">
+                <img src={logo} alt="Imagen de prueba" className="imagen mx-auto d-block" />
+              </div>
+              <div className="col-auto">
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+              </div>
+            </div>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav ms-auto">
-                <li className="nav-item">
-                  <a className="nav-link enlace" href="#">Contacto</a>
+                <li className="nav-item enlace">
+                  <a className="nav-link" href="/contacto">Contacto</a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link enlace" href="#">Telefono</a>
+                <li className="nav-item enlace">
+                  <a className="nav-link" href="/telefono">Telefono</a>
                 </li>
-                <li className="nav-item">
-                  <a className="nav-link enlace" href="#">Contactanos</a>
+                <li className="nav-item enlace">
+                  <a className="nav-link" href="/contactanos">Contactanos</a>
                 </li>
               </ul>
             </div>
